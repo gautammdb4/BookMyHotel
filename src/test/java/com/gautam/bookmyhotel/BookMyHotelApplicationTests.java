a0cfd047e82d630fa5976e987421866dee347c8a
Gautam Chaurasia <gautammdb4@gmail.com>
2025-01-24 18:02:15 +0530

    new project

---
+++ src/test/java/com/gautam/bookmyhotel/BookMyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautam.bookmyhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
